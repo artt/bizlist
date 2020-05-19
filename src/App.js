04850d2f890670c732f1f425aa63ed553d7e00ea
Art Tosborvorn <art.tosborvorn@gmail.com>
2020-05-19 16:47:06 +0700

    first phase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBox from './SearchBox'
+import Card from './Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      entries: [],
+      searchfield: ''
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+    console.log(this.searchfield)
+  }
+
+  render() {
+    console.log(this.state);
+    if (!this.state.entries.length) {
+      return <h1>Loading</h1>
+    }
+    else {
+      return (
+        <div className="App">
+          <h1>Hiiii</h1>
+          <Card name={this.state.entries[0].name.t} />
+          <SearchBox searchchange={this.onSearchChange} />
+        </div>
+      );
+    }
+  }
+
+  processResponse(response) {
+    let reducedResponse = JSON.parse(response.replace(/gsx\$|\$/g,'')).feed.entry.filter(entry => {return entry.display.t === 'x'});
+    this.setState({entries: reducedResponse.map(({call, color, email, facebook, fullname, keyword, line, name, remark, website}) => 
+                                                ({call, color, email, facebook, fullname, keyword, line, name, remark, website}))})
+  }
+
+  componentDidMount() {
+    fetch('https://spreadsheets.google.com/feeds/list/' + 
+        '1d9-1TwVhteX5xHlAdGvqhia6Lhcz4rwctGuBH6vDMgo/o9wnly8/public/values?alt=json')
+      .then(response => response.text())
+      .then(responsetext => this.processResponse(responsetext));
+  }
+
 }
 
 export default App;
