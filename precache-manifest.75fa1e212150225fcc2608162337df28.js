2a6b0daf9580dab4d0da598d8b8da7c92af89ace
Art Tosborvorn <art@nb140030846.bot.or.th>
2020-05-20 10:18:17 +0700

    Updates

---
+++ precache-manifest.75fa1e212150225fcc2608162337df28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd62a97efa71e71e4b604f401d0861e1",
+    "url": "/bizlist/index.html"
+  },
+  {
+    "revision": "e07c6e5f9b2e9d4d6f73",
+    "url": "/bizlist/static/css/main.a1b4103d.chunk.css"
+  },
+  {
+    "revision": "abfed5a43ad4a1dca074",
+    "url": "/bizlist/static/js/2.101c3aed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bizlist/static/js/2.101c3aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07c6e5f9b2e9d4d6f73",
+    "url": "/bizlist/static/js/main.466830f8.chunk.js"
+  },
+  {
+    "revision": "5e107f3524d480e3b723",
+    "url": "/bizlist/static/js/runtime-main.d8d443a4.js"
+  }
+]);
