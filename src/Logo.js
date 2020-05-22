0f03b95384b09d9f99daff1f74c864bf507713b7
Art Tosborvorn <art.tosborvorn@gmail.com>
2020-05-22 16:24:38 +0700

    logo

---
+++ src/Logo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Logo() {
+	return(
+		<div className='logo'>
+			B{/*
+				    */}<span class="special-i">
+	      <span class="stack">.</span>
+	      <span class="stack">ı</span>
+	    </span>{/*
+	    	    */}zL{/*
+	    	    	    */}<span class="special-i">
+	      <span class="stack bouncing">.</span>
+	      <span class="stack">ı</span>
+	    </span>{/*
+	    */}st
+		</div>
+	);
+}
+
+export default Logo;
