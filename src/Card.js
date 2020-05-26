fafad5e336a7545dd5bca8bdbda10e789f07224f
fishix10 <art.tosborvorn@gmail.com>
2020-05-26 22:57:58 +0700

    add fb + line

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,11 @@ import * as myutil from './myutil.js';
 function Card({entry}) {
 	return(
 		<div className='card'>
+			
+			{/* title */}
 			<div className='title'>{entry.fullname.t + (entry.remark.t && (' (' + entry.remark.t + ')'))}</div>
+			
+			{/* phone */}
 			<div>{ entry.call.t &&
 				entry.call.t.split('\n').map((item, i) => {
 					return (
@@ -17,20 +21,42 @@ function Card({entry}) {
 					);
 				})
 			}</div>
-			<div>{ entry.website.t &&
-				<a href={entry.website.t} target='_blank' rel='noopener noreferrer'>
-					<span className='website'>
-						{myutil.trimURL(entry.website.t)}
+
+			{/* website, facebook, line */}
+			<div>
+				{ entry.website.t &&
+					<a href={entry.website.t} target='_blank' rel='noopener noreferrer'>
+						<span className='website'>
+							{myutil.trimURL(entry.website.t)}
+						</span>
+					</a>
+				}
+			</div>
+
+			<div>
+				{ entry.facebook.t &&
+					<span className='facebook'>
+						<a href={entry.facebook.t} target='_blank' rel='noopener noreferrer'>
+							<i className="fab fa-facebook-square" /> facebook
+						</a>
 					</span>
-				</a>
-			}</div>
+				}
+				{ entry.facebook.t &&
+					<span className='line'>
+						&nbsp;<a href={`http://line.me/ti/p/${entry.line.t}`} target='_blank' rel='noopener noreferrer'>
+							<i className="fab fa-line" /> LINE
+						</a>
+					</span>
+				}
+			</div>
+
+			{/* colorbar */}
 			<div className='colorbar bar0'>
 				<svg>
 					<path d='M 250 0
 									 v 80
 									 h -250
 									 C 100 80, 250 80, 250 0'
-								stroke='none'
 								fill={`${entry.color.t}`} />
 				</svg>
 			</div>
@@ -40,7 +66,6 @@ function Card({entry}) {
 									 v 100
 									 h -150
 									 C 100 100, 150 50, 150 0'
-								stroke='none'
 								fill={`${entry.color.t}`} />
 				</svg>
 			</div>
@@ -50,7 +75,6 @@ function Card({entry}) {
 									 v 150
 									 h -100
 									 C 100 150, 100 20, 100 0'
-								stroke='none'
 								fill={`${entry.color.t}`} />
 				</svg>
 			</div>
